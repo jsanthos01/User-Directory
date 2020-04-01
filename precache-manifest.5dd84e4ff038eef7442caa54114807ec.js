91af8fb8940b3416439ed08a53828a42534af2ab
jsanthos01 <chinu.s2001@gmail.com>
2020-04-01 08:00:02 -0400

    Updates

---
+++ precache-manifest.5dd84e4ff038eef7442caa54114807ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1d1a68777b52c4bac149ee5b8789ca",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "2d45379f55dcba415c75",
+    "url": "/User-Directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5d9edf34f80a8f981da2",
+    "url": "/User-Directory/static/js/2.0a0a0843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.0a0a0843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d45379f55dcba415c75",
+    "url": "/User-Directory/static/js/main.09016044.chunk.js"
+  },
+  {
+    "revision": "bee9c9677fbf0c4ebd2e",
+    "url": "/User-Directory/static/js/runtime-main.307b0360.js"
+  }
+]);
